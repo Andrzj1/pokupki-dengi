29a4610934dcb7e6259e1f5eb6b8ffade200ae78
Andrzj1 <156209172+Andrzj1@users.noreply.github.com>
2024-07-03 19:49:06 +0300

    zaw_100

---
+++ html/main.js
@@ -0,0 +1,28 @@
+
+const  iPhone = document.querySelector(".iPhone");
+
+const samsung = document.querySelector(".samsung");
+
+iPhone.addEventListener("click", function (event) {
+
+let clickTarget = event.target;
+
+let activeButton = iPhone.querySelector(".active");
+
+let priceItem = iPhone.querySelector(".price");
+
+let currentPrice;
+
+let activeButtonColor = iPhone.querySelector("active-border");
+
+
+
+
+if (clickTarget.classList.contains("color-btn") && !clickTarget.classList.contains(active-border)) {
+
+ clickTarget.classList.add("active-border");
+
+activeButtonColor.classList.revove("active-border");
+}
+
+});
